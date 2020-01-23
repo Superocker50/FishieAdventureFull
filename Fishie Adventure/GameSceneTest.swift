b16a79c7369c03d9460098adacfdf1f2720f79e7
allanche <aca193@sfu.ca>
2020-01-22 20:56:21 -0800

    Update

---
+++ Fishie Adventure/GameSceneTest.swift
@@ -0,0 +1,39 @@
+//
+//  GameSceneTest.swift
+//  Fishie AdventureTests
+//
+//  Created by Allan Che on 2020-01-21.
+//  Copyright © 2020 Allan Che. All rights reserved.
+//
+
+import SpriteKit
+import XCTest
+
+class GameSceneTest: XCTestCase {
+
+    var player: SKSpriteNode?
+    var enemy: SKSpriteNode?
+    
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
